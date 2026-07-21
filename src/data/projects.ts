bf43fccaa11adafa5d36e661ec074bf1640cf84c
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2026-07-21 19:49:45 +0000

    Update projects.ts

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -166,7 +166,7 @@ export const projects: Project[] = [
       { key: 'Filtration', value: 'Multi-stage filtration' },
       { key: 'People Served', value: '500+ daily' },
     ],
-    featuredImage: '/images/hydrosolutions.jpg',
+    featuredImage: '/images/borehole1.jpg',
     galleryImages: [
       '/images/borehole1.jpg',
       '/images/borehole2.jpg',
