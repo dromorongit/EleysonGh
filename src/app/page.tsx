95e1e62ecb188f242c08aa0f1e1c88670f5270ef
Narh Henry Padi Dromor <hpdnarh@gmail.com>
2026-08-01 18:46:52 +0000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -237,7 +237,7 @@ return (
              </motion.div>
 
              <motion.div
-               className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6"
+               className="grid grid-cols-2 lg:grid-cols-4 gap-6"
                initial="initial"
                whileInView="animate"
                viewport={{ once: true }}
